6d9f04c26345bbe40a4f6cae349a863bbba6ee68
NR-288-42 <34372985+SxN13@users.noreply.github.com>
2019-07-05 18:43:23 +0400

    Время отображается корректно

--- gpu/gpu_openCL_alt/Source.cpp
+++ gpu/gpu_openCL_alt/Source.cpp
@@ -6,15 +6,14 @@
 #include <ctime>
 #include <cmath>
 #include <vector>
-
-#include "Prototype_test.h"
-
 //#ifdef __APPLE__
 //#include <OpenCL/opencl.h>
 //#else
 #define CL_USE_DEPRECATED_OPENCL_1_2_APIS
 #include <CL/cl.h>
 //#endif
+
+#include "Prototype_test.h"
 
 #define MAX_SOURCE_SIZE (0x1000000)
 
@@ -33,7 +32,7 @@ int main(void)
 	//Количество проходов в тесте
 	int turn = 1000;
 
-	// Create the two input vectors
+	// Входные вектора
 	int i;
 	const int LIST_SIZE = 1000;
 	int *A = (int*)malloc(sizeof(int)*LIST_SIZE);
@@ -44,7 +43,7 @@ int main(void)
 		B[i] = i + rand() % 10;
 	}
 
-	// Load the kernel source code into the array source_str
+	// Код кернела в source_str
 	FILE *fp;
 	char *source_str;
 	size_t source_size;
@@ -59,7 +58,7 @@ int main(void)
 	source_size = fread(source_str, 1, MAX_SOURCE_SIZE, fp);
 	fclose(fp);
 
-	// Get platform and device information
+	// Получение id платформ
 	cl_platform_id platform_id = NULL;
 	cl_device_id device_id = NULL;
 	cl_uint ret_num_devices;
@@ -67,32 +66,32 @@ int main(void)
 	cl_int ret = clGetPlatformIDs(1, &platform_id, &ret_num_platforms);
 	ret = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_GPU, 1, &device_id, &ret_num_devices);
 
-	// Create an OpenCL context
+	// Создания контекста OpenCL
 	cl_context context = clCreateContext(NULL, 1, &device_id, NULL, NULL, &ret);
 
-	// Create a command queue
+	// Создание очередь команд
 	//cl_command_queue command_queue = clCreateCommandQueue(context, device_id, 0, &ret);
 	cl_command_queue command_queue = clCreateCommandQueueWithProperties(context, device_id, 0, &ret);
 
-	// Create memory buffers on the device for each vector 
+	// Создание буферов для векторов 
 	cl_mem a_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY, LIST_SIZE * sizeof(int), NULL, &ret);
 	cl_mem b_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY, LIST_SIZE * sizeof(int), NULL, &ret);
 	cl_mem c_mem_obj = clCreateBuffer(context, CL_MEM_WRITE_ONLY, LIST_SIZE * sizeof(int), NULL, &ret);
 
-	// Copy the lists A and B to their respective memory buffers
+	// Перенос векторов в буферы для отправки на девайс
 	ret = clEnqueueWriteBuffer(command_queue, a_mem_obj, CL_TRUE, 0, LIST_SIZE * sizeof(int), A, 0, NULL, NULL);
 	ret = clEnqueueWriteBuffer(command_queue, b_mem_obj, CL_TRUE, 0, LIST_SIZE * sizeof(int), B, 0, NULL, NULL);
 
-	// Create a program from the kernel source
+	// Программа кернела
 	cl_program program = clCreateProgramWithSource(context, 1, (const char **)&source_str, (const size_t *)&source_size, &ret);
 
-	// Build the program
+	// Компиляция программы
 	ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
 
-	// Create the OpenCL kernel
+	// Создание кернела OpenCL
 	cl_kernel kernel = clCreateKernel(program, "vector_add", &ret);
 
-	// Set the arguments of the kernel
+	// Передача буферов в качестве аргумента
 	ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&a_mem_obj);
 	ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&b_mem_obj);
 	ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&c_mem_obj);
@@ -101,20 +100,22 @@ int main(void)
 	size_t global_item_size = LIST_SIZE; // Process the entire lists
 	size_t local_item_size = 64; // Divide work items into groups of 64
 	
+	int *C;
+
 	start_m = clock();
 	for (int i = 0; i < 1000; i++)
 	{
 		std::cout << i << "\n";
 		start = clock();
 		ret = clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, &global_item_size, &local_item_size, 0, NULL, NULL);
+		// Read the memory buffer C on the device to the local variable C
+		C = (int*)malloc(sizeof(int)*LIST_SIZE);
+		ret = clEnqueueReadBuffer(command_queue, c_mem_obj, CL_TRUE, 0, LIST_SIZE * sizeof(int), C, 0, NULL, NULL);
 		end = clock();
 		seconds = time_calculate(start, end);
 		time_vector.push_back(seconds);
 	}
 	end_m = clock();
-	// Read the memory buffer C on the device to the local variable C
-	int *C = (int*)malloc(sizeof(int)*LIST_SIZE);
-	ret = clEnqueueReadBuffer(command_queue, c_mem_obj, CL_TRUE, 0, LIST_SIZE * sizeof(int), C, 0, NULL, NULL);
 
 	// Отображение результатов
 	//for (i = 0; i < LIST_SIZE; i++)
